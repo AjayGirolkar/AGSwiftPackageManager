273e7325bb160cb083f159d3c6362499058c8c4e
ramanjaneyulukoduri <ramanjaneyulu.koduri@griffithuni.edu.au>
2022-05-28 17:38:20 +0530

    Added DemoView

---
+++ AGSwiftPackage/Views/DemoView.swift
@@ -0,0 +1,23 @@
+//
+//  DemoView.swift
+//  AGSwiftPackageManager
+//
+//  Created by Ajay Girolkar on 28/05/22.
+//
+import SwiftUI
+
+public struct DemoView: View {
+    
+    public init () {
+        
+    }
+    public var body: some View {
+        Text("Hello, World!, This is library")
+    }
+}
+
+struct DemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoView()
+    }
+}
